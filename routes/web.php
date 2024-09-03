de1c0eb36c5bd6e0cbd85e3d6b09cddcd6a3ddcd
usamaJ17 <usamajalal17@gmail.com>
2024-09-03 18:37:34 +0500

    upd

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin/certificates');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('admin/import', [App\Http\Controllers\CertificatesController::class, 'importForm'])->name('import.form');
-Route::post('admin/import', [App\Http\Controllers\CertificatesController::class, 'importData'])->name('import.excel');
+    Route::get('admin/import', [App\Http\Controllers\CertificatesController::class, 'importForm'])->name('import.form');
+    Route::post('admin/import', [App\Http\Controllers\CertificatesController::class, 'importData'])->name('import.excel');
+    
+    Route::get('admin/certificates', [App\Http\Controllers\CertificatesController::class, 'index'])->name('admin.certificates.view');    
+});
 
-Route::get('admin/certificates', [App\Http\Controllers\CertificatesController::class, 'index'])->name('admin.certificates.view');
